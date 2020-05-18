5cdabe33e5bf9d65c5179d14c11f7b2120223152
Macarena Pecha <macupecha@hotmail.com>
2020-05-18 13:52:40 -0300

    add method 3

--- app.js
+++ app.js
@@ -27,5 +27,37 @@ let rootDir = fs.readdirSync('./'); //muestra los archivos de esa ruta en forma 
 fs.readdir('./', ((err, files) => { //muestra los archivos de esa ruta en forma Sincrona
   (err) ? console.log(err) : console.log('async' ,files);
 }));
-
 console.log('sync' ,rootDir);
+
+
+// 1 - Create a method to get all the files inside a given path
+// 2 - Create a method to Print all the files
+// 3 - Create a method to sort and print the files
+// 4 - Create a method to get the amount of files starting with one letter
+function showFiles(path){
+  fs.readdir(path, (err, files) => { //muestra los archivos de esa ruta en forma Asincrona
+    (err) ? console.log(err) : console.log('showFiles' ,files); // averiguar async await
+  });
+}
+let show = showFiles('C:\\Program Files');
+
+function sortFiles(path){
+  fs.readdir(`${path}`, (err, files) => { //muestra los archivos de esa ruta en forma Asincrona
+    (err) ? console.log(err) : console.log('sortFiles', files.reverse());
+  });
+}
+let sort = sortFiles('C:\\Program Files');
+
+function amountFiles(path, letter){
+  let amount=0;
+  fs.readdir((path), (err, files) => { //muestra los archivos de esa ruta en forma Asincrona
+    if(err) return console.log(err);
+      files.forEach( (file) => {
+        if(file.substring(0, 1) === letter.toUpperCase()) {
+          amount++;
+        }
+      });
+    console.log(amount, 'amount');
+  });
+}
+let amount = amountFiles('C:\\Program Files', 'm');
