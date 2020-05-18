5156dd9dcee27bbe8a6756e544f40164068a9136
Macarena Pecha <macupecha@hotmail.com>
2020-05-18 08:46:14 -0300

    first commit Maka

---
+++ app.js
@@ -0,0 +1,9 @@
+// let sayHello = (name) => {
+//   console.log("Hello " + name);
+// };
+//
+// sayHello("Maka");
+const logger = require('./logger'); //esta es la forma de pedir el logger
+
+logger.log('Maka');
+console.log(logger);
---
+++ logger.js
@@ -0,0 +1,8 @@
+let url = 'http://myurl.com';
+
+log = (message) => {
+  console.log('Hello ', message);
+};
+
+// module.exports.url = url; //aca se exporta de a uno
+module.exports.log = log; //aca se exporta de a uno
